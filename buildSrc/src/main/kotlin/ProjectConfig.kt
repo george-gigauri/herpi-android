95ebb24e2b6cc74587ecb3867d24e063bf7ba794
george-gigauri <giorgi.gigauri.1@btu.edu.ge>
2025-08-11 22:02:53 +0400

    Update project configuration
    This commit updates the project configuration:
    - Incremented `versionCode` to 40.
    - Incremented `versionName` to "3.0.16".

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -4,8 +4,8 @@ object ProjectConfig {
     const val minSdk = 21
     const val targetSdk = 36
 
-    const val versionCode = 39
-    const val versionName = "3.0.15"
+    const val versionCode = 40
+    const val versionName = "3.0.16"
 
     const val isMinifyEnabled = false
 }
